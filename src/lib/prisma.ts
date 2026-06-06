c311d94b0c4146f78cb38521e3dc6c45c9031885
NitalAgrawal <nitalagrawal5@gmail.com>
2026-06-06 14:47:13 +0530

    prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,17 @@
-import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
-
-const adapter = new PrismaNeon({
-  connectionString: process.env.DATABASE_URL!,
-});
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaNeon({
+      connectionString: process.env.DATABASE_URL!,
+    }),
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
