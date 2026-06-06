91bfffb4d0dbddde0fc2da66f15c4e81f283f81b
NitalAgrawal <nitalagrawal5@gmail.com>
2026-06-06 15:13:16 +0530

    fix: prisma adapter simplified

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,16 @@
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { Pool } from "@neondatabase/serverless";
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const { PrismaClient } = require("@prisma/client");
-
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaNeon(pool);
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const globalForPrisma = globalThis as unknown as { prisma: any };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const prisma: any =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaNeon({ connectionString: process.env.DATABASE_URL }),
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
